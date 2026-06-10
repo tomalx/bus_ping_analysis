6a48d16422b4ad158027a166806a2538bf2355c8
tomalx <tomalx14@gmail.com>
2026-06-10 11:16:45 +0100

    Update 00_parquet_store.R

--- r/00_parquet_store.R
+++ r/00_parquet_store.R
@@ -2,39 +2,52 @@
 
 library(arrow)
 library(tidyverse)
-
-bus_loc_csv <- dir(path = "csv/", pattern = ".csv", full.names = TRUE)
-bus_loc_csv <- map(bus_loc_csv[9:10],read.csv)
 library(purrr)
+
+## *TODO* - need to find way of writing parquet in batch without overwriting
+##          records  from previous batch
+
+
+# *WARNING* - make sure you're in the right directory
+#  there are several csv folders
+#  you need 03 Analysis Projects/bod_api/bus_ping_analysis
+bus_loc_csv <- dir(path = "csv/", pattern = ".csv", full.names = TRUE)
+bus_loc_csv
+bus_loc_csv <- map(bus_loc_csv[10:17],read.csv)
 
 #convert to date-time using lubridate
 # mutate(time = ymd_hms(time)) %>% 
 bus_loc_csv <- map(bus_loc_csv, ~ .x %>% 
-            mutate(trip_start = ymd_hms(originAimedDepatureTime)))
+            mutate(time = ymd_hms(time)))
 
 
 
 # add day week or month attribute
 bus_loc_csv <- map(bus_loc_csv, ~ .x %>% 
-                     mutate(month = as.character(month(trip_start))))
+                     mutate(day = as.character(day(time))))
 bus_loc_csv <- map(bus_loc_csv, ~ .x %>% 
-                     mutate(year = as.character(year(trip_start))))
+                     mutate(month = as.character(month(time))))
 bus_loc_csv <- map(bus_loc_csv, ~ .x %>% 
-                     mutate(year_month = paste0(year,month)))
+                     mutate(year = as.character(year(time))))
+bus_loc_csv <- map(bus_loc_csv, ~ .x %>% 
+                     mutate(year_month_day = paste0(year,month,day)))
 
 
 # write to parquet
 bus_loc_csv <- do.call(rbind, bus_loc_csv)
   
 # write to parquet
+parquet_month_dir <- as.character(paste0(year(Sys.Date()),month(Sys.Date())))
+
 
 bus_loc_csv %>% 
-  group_by(year_month) %>% 
-  write_dataset(path = "parquet", format = "parquet")
+  group_by(year_month_day) %>% 
+  write_dataset(path = paste0("parquet/",parquet_month_dir), format = "parquet")
 
 tibble(
-  files = list.files("parquet", recursive = TRUE),
-  size_MB = file.size(file.path("parquet", files)) / 1024^2
+  files = list.files(path = paste0("parquet/",parquet_month_dir), recursive = TRUE),
+  size = file.size(file.path(path = paste0("parquet/",parquet_month_dir), files)),
+  size_MB = file.size(file.path(path = paste0("parquet/",parquet_month_dir), files)) / 1024^2
 )
 
 
