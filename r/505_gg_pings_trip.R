9af238963492e284bce521fe210a734693a536b9
tomalx <tomalx14@gmail.com>
2026-07-07 16:14:23 +0100

    multi bus anim

--- r/505_gg_pings_trip.R
+++ r/505_gg_pings_trip.R
@@ -19,16 +19,19 @@ route_buffer <- dc_routes %>%
 inverse <- st_difference(st_bbox(route_buffer) %>% st_as_sfc(), route_buffer)
 inverse_plus <- st_difference(st_bbox(route_buffer) %>% st_as_sfc(), st_buffer(route_buffer, -200))
 
-pings_unq_trip <- ping_sample_one_route(route_number = "1")
+pings_unq_trip <- ping_sample_one_route(pings = pings_in_min, route_number = "1")
 pings_unq_trip_id <- pings_unq_trip %>% 
-  filter(directionRef == "inbound") %>% 
+  #filter(directionRef == "inbound") %>% 
   filter(hour(time) == 8) %>% 
   pull(journeyCodeUnq) %>% 
-  unique() %>% 
-  sample(1)
+  unique() #%>% 
+  #sample(2)
 pings_unq_trip <- pings_unq_trip %>%
-  filter(journeyCodeUnq == pings_unq_trip_id)
-  st_transform(crs = 3857)
+  #filter(journeyCodeUnq %in% pings_unq_trip_id) %>% 
+  group_by(journeyCodeUnq) %>% 
+  filter(n() > 60) %>% 
+  st_transform(crs = 3857) # %>% 
+  #ungroup()
   
 
 
@@ -100,25 +103,29 @@ ggsave(filename = glue::glue("qmd/pings_unq_route_focus.jpeg"),
 pings_anim <- 
   route_basemap +
   ggplot2::geom_sf(data = pings_unq_trip %>% st_transform(3857),
-                   #aes(group = time),
-                   colour = pal[2],
+                   aes(color = journeyCodeUnq),
+                   #colour = pal[2],
                    linewidth = NA,
-                   size = 2,
-                   alpha = 0.8)
+                   size = 4,
+                   alpha = 0.8) +
+  scale_colour_manual(values=pal)
 
 pings_anim <- pings_anim +
-  transition_states(time, transition_length = 1, state_length = 0.1) +
-  shadow_wake(0.1, size = 1, alpha = 0.2) +
+  transition_states(time, transition_length = 1, state_length = 1) +
+  shadow_wake(0.9, size = 1, alpha = 0.2) +
   labs( subtitle = "Time: {frame_along}" ) 
+  
   #transition_time(time = time) +
   #shadow_mark(past = TRUE)
 
-animate( pings_anim, duration = 2, detail = 0.5)
+animate( pings_anim, fps = 10, nframes = 30, duration = 5)
   
-anim_save(file = "gif/pings_anim2.gif",
+anim_save(file = "gif/pings_anim_multi.gif",
           plot = get_last_plot(),
           dpi = 320,
           width = 12,
           height = 8,
           units = "cm")
+
+
 
