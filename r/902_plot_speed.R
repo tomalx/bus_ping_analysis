ebb1668762d1ff2c2df842aa385ab3cf8845246d
tomalx <tomalx14@gmail.com>
2026-07-10 15:33:11 +0100

    bin speed

--- r/902_plot_speed.R
+++ r/902_plot_speed.R
@@ -1,12 +1,44 @@
 ## plot ping speed
 
-pings_plot <- pings %>% 
-  ping_filter() %>% 
+# stops with dist_m
+stops_0 <- stop_seq %>% 
+  ungroup() %>% 
+  st_as_sf() %>% 
+  st_transform(4326) %>%
   filter(direction_id == 0) %>% 
+  #group_by(journeyCode, day, month) %>% 
+  mutate(dist_m = route_distance_calc(., routes = dc_routes, longest_stop_seq = longest_stop_seq, density = 0.5))
+
+stops_1 <- stop_seq %>% 
+  ungroup() %>% 
+  st_as_sf() %>% 
+  st_transform(4326) %>%
+  filter(direction_id == 1) %>% 
+  #group_by(journeyCode, day, month) %>% 
+  mutate(dist_m = route_distance_calc(., routes = dc_routes, longest_stop_seq = longest_stop_seq, density = 0.5))
+
+
+
+pings
+
+dist_m_bin_size <- 50 ## size in metres
+
+
+pings_plot <- pings %>% 
+  ping_filter(direction = 1, sample_jnycode = 10) %>% 
+
  # filter(stringr::str_starts(journeyCode, pattern = "08")) %>% 
   ping_speed() %>% 
   # mutate(dist_m_bin =  cut(dist_m, breaks = 2)) 
-  mutate(dist_m_bin = ntile(dist_m, n = 200))
+  mutate(dist_m_bin = cut(dist_m, 
+                          breaks = c(seq(0, max(dist_m), dist_m_bin_size)), 
+                          labels = c(seq(dist_m_bin_size, max(dist_m), dist_m_bin_size)) )) 
+
+pings_plot <- pings_plot %>% 
+  group_by(journeyCodeUnq, dist_m_bin) %>% 
+  mutate(sample_size = n()) %>%
+  mutate(bin_speed = sum(ping_speed)/n()) %>% 
+  ungroup()
 
 pings_plot$dist_m_bin <- format(pings_plot$dist_m_bin, scientific = FALSE)
 
@@ -31,5 +63,5 @@ pings_plot %>% ggplot(
 ) +
   geom_line(alpha = 0.5, stroke = NA, size = 0.1, color = "#ababab") +
   theme_minimal() +
-  geom_vline(xintercept = c(1000))
+  geom_vline(xintercept = stops_1$dist_m)
 
