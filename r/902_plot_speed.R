921fca0c6059c3018c0acb6011cefab2feaac392
tomalx <tomalx14@gmail.com>
2026-07-14 15:43:30 +0100

    Update 902_plot_speed.R

--- r/902_plot_speed.R
+++ r/902_plot_speed.R
@@ -17,15 +17,23 @@ stops_1 <- stop_seq %>%
   #group_by(journeyCode, day, month) %>% 
   mutate(dist_m = route_distance_calc(., routes = dc_routes, longest_stop_seq = longest_stop_seq, density = 0.5))
 
-
+route_stop_split <- split_at_stop(stop_seq = stops_0,
+                          routes = dc_routes,
+                          longest_stop_seq = longest_stop_seq
+)
 
 pings
 
+# breaks by a set distance
 dist_m_bin_size <- 400 ## size in metres
+
+# breaks by stop to stop distance
+seg_breaks <- route_stop_split$dist_m_start
+seg_names <- stops_0$stop_name[2:length(stops_0$stop_name)]
 
 
 pings_plot <- pings %>% 
-  ping_filter(direction = 1, 
+  ping_filter(direction = 0, 
               hr_of_day = c(0:23) , 
               sample_jnycode = 25) %>%
   group_by(journeyCodeUnq) %>% 
@@ -36,9 +44,14 @@ pings_plot <- pings %>%
  # filter(stringr::str_starts(journeyCode, pattern = "08")) %>% 
   ping_speed() %>% 
   # mutate(dist_m_bin =  cut(dist_m, breaks = 2)) 
-  mutate(dist_m_bin = cut(dist_m, 
-                          breaks = c(seq(0, max(dist_m), dist_m_bin_size)), 
-                          labels = c(seq(dist_m_bin_size, max(dist_m), dist_m_bin_size)) )) 
+  mutate(dist_m_bin = cut(dist_m,
+                          breaks = seg_breaks,
+                          labels = seg_names
+                          #breaks = c(seq(0, max(dist_m), dist_m_bin_size)),   ### Breaks by equal size (option)
+                          #labels = c(seq(dist_m_bin_size, max(dist_m), dist_m_bin_size)) 
+                          )) %>% 
+  #remove rows with NA values
+  filter(!is.na(dist_m_bin))
 
 pings_plot <- pings_plot %>% 
   group_by(journeyCodeUnq, dist_m_bin) %>% 
@@ -58,7 +71,9 @@ pings_plot %>% ggplot(
 pings_plot %>% ggplot(
   aes(x = dist_m_bin, y = ping_speed)
 ) +
-  geom_boxplot()# +
+  geom_boxplot() +
+  ylim(c(0,25)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
   #scale_x_binned()
 
 pings_plot %>% ggplot(
