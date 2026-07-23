d3f1349b54c5be09830d214dae56d29a4ecf7848
tomalx <tomalx14@gmail.com>
2026-07-23 15:30:23 +0100

    Update 520_patch_map_box.R

--- r/520_patch_map_box.R
+++ r/520_patch_map_box.R
@@ -2,7 +2,11 @@
 
 
 map <- ggplot() +
-  geom_sf_interactive(data = pings_seg_speed, mapping = aes(colour = speed_50, tooltip = round(speed_50,2), data_id = seg_name), size = 2) +
+  geom_sf_interactive(data = pings_seg_speed, 
+                      mapping = aes(colour = speed_50, 
+                                    #tooltip = round(speed_50,2), 
+                                    data_id = seg_name), 
+                      size = 4) +
  # scale_color_brewer() +
   geom_sf_interactive(data = stops_1, aes(tooltip = stop_name)) +
   theme_void() +
@@ -25,61 +29,44 @@ map <- ggplot() +
     )
   )
 
-  
-  scale_colour_paletteer_c(
-    "viridis::plasma",
-   # breaks = rev(brks_scale),
-   # labels = labels_scale,
-    guide = guide_colourbar(
-      title.position = 'top',
-      direction = "horizontal"
-    )
-  ) 
+ 
 map
   
   
-  scale_colour_paletteer_c("ggthemes::Green-Gold",
-                                )
-
-  # scale_fill_paletteer_c("MoMAColors::Flash",
-  #   
-  #   breaks = rev(brks_scale),
-  #   name = "Average age",
-  #   drop = FALSE,
-  #   labels = labels_scale,
-  #   guide = guide_legend(
-  #     direction = "horizontal",
-  #     keyheight = unit(2, units = "mm"), 
-  #     keywidth = unit(70/length(labels), units = "mm"),
-  #     title.position = 'top',
-  #     title.hjust = 0.5,
-  #     label.hjust = 1,
-  #     nrow = 1,
-  #     byrow = TRUE,
-  #     reverse = TRUE,
-  #     label.position = "bottom"
-  #   )
-  # )
-
-map
+ 
 
 ## gg boxplot
 
 boxplot <- pings_plot %>%
   group_by(seg_name) %>% 
-  mutate(ping_speed_median = median(ping_speed)) %>% 
+  mutate(
+    ping_speed_median = median(ping_speed, na.rm = TRUE),
+    ping_speed_mean   = mean(ping_speed, na.rm = TRUE),
+    ping_speed_iqr    = IQR(ping_speed, na.rm = TRUE),
+    ping_speed_sd     = sd(ping_speed, na.rm = TRUE),
+    ping_count        = n(),
+    ping_count_lt1    = sum(ping_speed < 1, na.rm = TRUE),
+    tooltip_text = paste0(
+      "<b>", seg_name, "</b><br/>",
+      "Median: ", round(ping_speed_median, 2), " m/s<br/>",
+      "Mean: ", round(ping_speed_mean, 2), " m/s<br/>",
+      "IQR: ", round(ping_speed_iqr, 2), "<br/>",
+      "SD: ", round(ping_speed_sd, 2), "<br/>",
+      "Pings: ", ping_count, "<br/>",
+      "&lt;1 m/s: ", ping_count_lt1
+    )
+    ) %>% 
   ungroup() %>% 
   ggplot(
   aes(x = forcats::fct_rev(seg_name), y = ping_speed, group = seg_name, fill = ping_speed_median)
 ) +
-  geom_boxplot_interactive(aes(data_id = as.character(seg_name), tooltip = seg_name), outliers = FALSE) +
+  geom_boxplot_interactive(aes(data_id = as.character(seg_name), tooltip = tooltip_text), outliers = FALSE) +
   ylim(c(0,25)) +
   
   #scale_x_reverse() +
   coord_flip() +
   theme_void() +
-  theme(axis.text.y = element_text(size = 7, 
-                                   vjust = 0.5, hjust=1),
+  theme(# axis.text.y = element_text(size = 7,vjust = 0.5, hjust=1),
         legend.position = "bottom") 
 #scale_x_binned()
 
@@ -94,9 +81,32 @@ girafe(ggobj = map | boxplot,
          opts_hover(css = ''),
          opts_hover_inv(css = 'opacity:0.1;'),
          opts_tooltip(
+           offx = 20,
+           offy = -50,
            css = 'background:white; border:1px solid grey;'
          )
        ))
+
+# render with fixed position tooltip:
+girafe(
+  ggobj = map | boxplot,
+  width_svg = 18,
+  height_svg = 12,
+  options = list(
+    opts_hover(css = ''),
+    opts_hover_inv(css = 'opacity:0.1;'),
+    opts_tooltip(
+      use_fill = TRUE,
+      offx = 0,
+      offy = 0,
+      css = "
+    background:white;
+    border:1px solid grey;
+    padding:5px;
+  "
+    )
+  )
+)
 
 
 
